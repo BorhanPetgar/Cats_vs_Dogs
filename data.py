7a5905472e5c7d758bbec325cef788dc45d18231
BorhanPetgar <borhanpetgar40@gmail.com>
2023-07-13 11:34:23 +0330

    class created

--- data.py
+++ data.py
@@ -3,3 +3,10 @@ from torch.utils.data import Dataset, DataLoader
 import glob
 
 
+class Cats_vs_Dogs_Data(Dataset):
+    def __init__(self, folder_path):
+        pass
+    def __len__(self):
+        pass
+    def __getitem__(self, index):
+        pass
